d37b1a6f02c7a5956c188d029c06616a146f0a6f
Gino <genebaybay@yahoo.com>
2020-11-01 22:03:28 -0500

    requirements for database design

--- server/models/users.js
+++ server/models/users.js
@@ -1,18 +1,28 @@
 /* B"H
 */
+const mysql = require('./mysql');
 
-const data = [{ name: 'Moshe', age: 43}, { name: 'Biden', age: 78 }]
+//const data = [{ name: 'Moshe', age: 43}, { name: 'Biden', age: 78 }]
 
 async function getAll(){
     //throw { status: 501, message: "This is a fake error" }
     //await Promise.resolve()
     console.log("Called Get All")
-    return data;
+    return await mysql.query(`SELECT * FROM Users`);
+}
+
+async function get(id){
+    return await mysql.query(`SELECT * FROM Users WHERE id=?`, [id]);
+}
+
+async function getTypes(){
+    return await mysql.query(`SELECT id, Name FROM Types WHERE Type_id = 2`);
 }
 
 async function add(name, age){
     data.push({name, age});
 }
 
+const search = async q => await mysql.query(`SELECT id, FirstName, LastName FROM Users WHERE LastName LIKE ? OR FirstName LIKE ?; `, [`%${q}%`, `%${q}%`]);
 
-module.exports = { getAll, add, search: async q => data.filter(x=> x.name == q) }
+module.exports = { getAll, get, add, getTypes, search }
