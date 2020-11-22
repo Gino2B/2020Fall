60b182af6b0313c6e7a7febed613c51c02e10ef3
Gino <genebaybay@yahoo.com>
2020-11-22 04:00:28 -0500

    client changes

--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -1,38 +1,44 @@
 <template>
-    <div class="page">
+  <div class="page">
       <h2 class="title is-2">Feed Page</h2>
-        
+
         <div class="columns">
-            <div class="column is-two-thirds">
-                <Post v-for=" (x, i) in posts " 
-                    :key="i"
-                    :post="x" />
+            <div class="column is-one-quarter" @mouseenter="error">
+                <Sidebar />
             </div>
-            <div class="column is-one-third" @mouseenter="error">
+            <div class="column is-one-half">
+                <Post v-for=" (x, i) in posts " 
+                      :key="i"
+                      :i="i"
+                      :post="x" /> 
+            </div>
+            <div class="column is-one-quarter" @mouseenter="error">
                 <Sidebar />
             </div>
         </div>
-    </div>
+  </div>
 </template>
 
 <script>
 import Sidebar from "@/components/Sidebar";
 import Post from "@/components/Post";
-import { posts } from "@/models/feed";
+import { getPosts } from "@/models/feed";
 import session from "@/models/session";
-
 export default {
     data(){
         return {
-            posts
+            posts: []
         }
     },
+    async created(){
+        this.posts = await getPosts();
+    },
     components: {
-        Sidebar,Post
+        Sidebar, Post
     },
     methods: {
-        error() {
-            session.addNotification('Something went wrong.', 'danger')
+        error(){
+            //session.addNotification('Something went wrong.', 'danger')
         }
     }
 }
@@ -40,6 +46,6 @@ export default {
 
 <style>
     .card {
-        margin-bottom: 20px;
+        margin-bottom: 30px ;
     }
 </style>
